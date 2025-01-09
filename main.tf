5155bdf82265fc5c82f2b6425d8c40ed361dfbbc
Christopher Goodwin <christophergoodwin@ChristodwinsMBP.localdomain>
2025-01-09 14:13:11 -0800

    testing csp

--- main.tf
+++ main.tf
@@ -36,5 +36,6 @@ resource "google_compute_backend_bucket" "site" {
   project     = var.project_id
   description = "Backend service for ${var.name}"
   bucket_name = google_storage_bucket.site.name
+  custom_response_headers = ["Content-Security-Policy: default-src 'self' https:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' 'https://greenroom-assets-${var.project_id}.greenroom-${var.project_id}.com' data:; font-src 'self'; connect-src 'self'; frame-src 'self'; media-src 'self'"]
   enable_cdn  = true
 }
